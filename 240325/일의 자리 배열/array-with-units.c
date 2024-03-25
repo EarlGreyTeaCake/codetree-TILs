17e2152df18fb7e0f772253ba53f2ef56591a6c0
Eun Bee Kim <76466677+EarlGreyTeaCake@users.noreply.github.com>
2024-03-25 16:42:51 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240325/일의 자리 배열/array-with-units.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    int arr[10] = {a, b};
+
+    for(int i = 2; i < 10; i++)
+    {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
