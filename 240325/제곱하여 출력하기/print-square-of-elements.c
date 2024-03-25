aeee30d53b093639b557afacd1b4b3773ca605ab
Eun Bee Kim <76466677+EarlGreyTeaCake@users.noreply.github.com>
2024-03-25 16:25:22 +0900

    Title: 제곱하여 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240325/제곱하여 출력하기/print-square-of-elements.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int a, b, c;
+
+    scanf("%d \n", &n);
+    scanf("%d %d %d", &a, &b, &c);
+
+    int arr[100] = {a, b, c};
+
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] *= arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+
+    return 0;
+}
