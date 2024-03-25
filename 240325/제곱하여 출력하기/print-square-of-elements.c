5b372fd16b7f378565a65737892a7ead9a1ec3a1
Eun Bee Kim <76466677+EarlGreyTeaCake@users.noreply.github.com>
2024-03-25 16:31:18 +0900

    Title: 제곱하여 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240325/제곱하여 출력하기/print-square-of-elements.c
+++ 240325/제곱하여 출력하기/print-square-of-elements.c
@@ -4,22 +4,23 @@ int main() {
     int n;
     unsigned int k;
 
-    scanf("%d \n", &n);
+    scanf("%d \n", &n); // 우선 배열의 크기가 될 값 n 받기
 
-    int arr[100];
+    int arr[100]; // 배열 선언
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++) // scanf로 입력받을 크기만큼의 입력받기
     {
         scanf("%d ", &k);
         arr[i] = k;
     }
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++) // 제곱
     {
         arr[i] *= arr[i];
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) // 프린트
+    {
 		printf("%d ", arr[i]);
 	}
 
