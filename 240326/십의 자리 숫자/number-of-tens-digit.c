289a98e4d51628e7609478a672b691e2f43f70fa
Eunbee Kim <76466677+EarlGreyTeaCake@users.noreply.github.com>
2024-03-26 19:13:58 +0900

    Title: 십의 자리 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240326/십의 자리 숫자/number-of-tens-digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[100];
+
+    int count_arr[100] = { 0, }; // 0번째는 비움
+
+    while(1)
+    {
+        int i = 0;
+
+        scanf("%d ", &arr[i]);
+
+        if(arr[i] > 9) // 10 이상만
+        {
+            n = arr[i] / 10; // 십의 자리 얻기
+            count_arr[n]++; // 같은 숫자 카운트
+        }
+
+        if(arr[i] == 0) // 0 나오면 끝
+        {
+            break;
+        }
+
+        i++;
+    }
+
+    // 개수 출력
+	for (int i = 1; i < 10; i++) 
+    {
+		printf("%d - %d \n", i, count_arr[i]);
+	}
+
+    return 0;
+}
